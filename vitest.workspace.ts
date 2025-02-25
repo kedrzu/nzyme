afb4a4e28890e67b9c774241b17e25300ed3b5f6
Michał Kędrzyński <kedrzu@gmail.com>
2025-02-25 12:36:15 +0100

    chore: update test and eslint

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,3 +1,7 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineWorkspace } from 'vitest/config';
 
-export default defineWorkspace(['./packages/*/vitest.config.ts']);
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default defineWorkspace([path.join(dirname, 'packages/*/vitest.config.ts')]);

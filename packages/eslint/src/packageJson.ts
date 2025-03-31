8f02260a6241ba5a5444b0b4f139ce7de6b726b1
Michał Kędrzyński <kedrzu@gmail.com>
2025-03-31 14:36:51 +0200

    fix(eslint): fix jsdoc

--- packages/eslint/src/packageJson.ts
+++ packages/eslint/src/packageJson.ts
@@ -1,5 +1,13 @@
 import packageJsonPlugin from 'eslint-plugin-package-json';
 
 export function packageJson() {
-    return [packageJsonPlugin.configs.recommended];
+    return [
+        packageJsonPlugin.configs.recommended,
+        {
+            rules: {
+                'package-json/require-name': 'off',
+                'package-json/require-version': 'off',
+            },
+        },
+    ];
 }

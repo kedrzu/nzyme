6f1f20b5df0ab9ef169cd5b445c4dedac444a87c
Michał Kędrzyński <kedrzu@sigma.clinic>
2025-09-26 10:16:02 +0200

    feat(vue-utils): add navigateToUrl function

--- packages/vue-utils/src/index.ts
+++ packages/vue-utils/src/index.ts
@@ -14,6 +14,7 @@ export * from './directives/vShow.js';
 export * from './directives/vUid.js';
 export * from './directives/vVisible.js';
 export * from './LoadingContext.js';
+export * from './navigateToUrl.js';
 export * from './onElementScroll.js';
 export * from './onEventEmitter.js';
 export * from './onHistoryBack.js';

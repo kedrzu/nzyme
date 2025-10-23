c922e3c10632410c16018ae190950b0e2bd1643c
Michał Kędrzyński <kedrzu@sigma.clinic>
2025-10-23 09:52:40 +0200

    feat(fetch-utils): added getResponseFileName util

--- packages/apis/src/inpost.ts
+++ packages/apis/src/inpost.ts
@@ -1,7 +1,6 @@
-export * from './inpost/InpostPointList.js';
 export * from './inpost/InpostPointGet.js';
+export * from './inpost/InpostPointList.js';
 export * from './inpost/InpostTrackingGet.js';
-
 export * from './inpost/models/InpostPointData.js';
 export * from './inpost/models/InpostStatus.js';
 export * from './inpost/models/InpostTrackingData.js';

4c03d4b61d3dca87dd7dc9f0c21f88ea9eb5ae32
Michał Kędrzyński <kedrzu@sigma.clinic>
2025-10-30 13:06:00 +0100

    feat(i18n): support german pluralization

--- packages/i18n-core/src/pluralization.ts
+++ packages/i18n-core/src/pluralization.ts
@@ -28,59 +28,6 @@ export type PluralTranslation<TPluralization extends Pluralization, TParams exte
 };
 
 /**
- * English pluralization
- */
-export const englishPluralization = definePluralization({
-    plurals: ['many', 'one', 'zero'],
-    optionalPlurals: ['zero'],
-    pluralize: (count, plurals) => {
-        switch (count) {
-            case 0:
-                return plurals.zero ?? plurals.many;
-            case 1:
-                return plurals.one;
-            default:
-                return plurals.many;
-        }
-    },
-});
-
-/**
- * Polish pluralization
- */
-export const polishPluralization = definePluralization({
-    plurals: ['few', 'many', 'one', 'zero'],
-    optionalPlurals: ['zero'],
-    pluralize: (count, plurals) => {
-        // np: zero jabłek
-        if (count === 0) {
-            return plurals.zero ?? plurals.many ?? plurals.few;
-        }
-
-        // np: jedno jabłko
-        if (count === 1) {
-            return plurals.one;
-        }
-
-        if (count < 10 || count > 20) {
-            // np: 3 jabłka lub 23 jabłka
-            const lastDigit = count % 10;
-            if (lastDigit >= 2 && lastDigit <= 4) {
-                return plurals.few ?? plurals.many;
-            }
-        }
-
-        // np 8 jabłek
-        return plurals.many ?? plurals.few;
-    },
-});
-
-/**
- * Czech pluralization
- */
-export const czechPluralization = polishPluralization;
-
-/**
  * Define a pluralization method
  * @param pluralization - Pluralization
  * @returns Pluralization

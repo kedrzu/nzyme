b1cfe8dfc34c8e458d540bccdbd3e0ece13c072d
Michał Kędrzyński <kedrzu@sigma.clinic>
2025-11-03 14:12:00 +0100

    feat(pulumi): add createCloudfrontDnsRecords function

--- packages/pulumi/src/aws.ts
+++ packages/pulumi/src/aws.ts
@@ -1,4 +1,5 @@
 export * from './aws/createApiGatewayCloudfrontOrigin.js';
+export * from './aws/createCloudfrontDnsRecords.js';
 export * from './aws/createDnsValidatedCertificate.js';
 export * from './aws/RotatedSecret.js';
 export * from './aws/types.js';

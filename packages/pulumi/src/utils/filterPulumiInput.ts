6f2cfceb0d1b3300701ef5bd4ccb71f65c9b4ecc
Michał Kędrzyński <kedrzu@sigma.clinic>
2025-11-25 14:26:13 -0800

    [SIG-231] Submodule changes

--- packages/pulumi/src/utils/filterPulumiInput.ts
+++ packages/pulumi/src/utils/filterPulumiInput.ts
@@ -1,10 +1,10 @@
 import * as pulumi from '@pulumi/pulumi';
 
 /**
- * Filters out null and undefined values from an array.
+ * Filters out false, null and undefined values from an array.
  */
 export function filterPulumiInput<T>(input: pulumi.Input<T[]>): pulumi.Input<Exclude<T, false | null | undefined>[]> {
     return pulumi
         .all([input])
-        .apply(([input]) => (input?.filter(Boolean) ?? []) as Exclude<T, false | null | undefined>[]);
+        .apply(([input]) => (input?.filter((item): item is Exclude<T, false | null | undefined> => item !== false && item != null) ?? []) as Exclude<T, false | null | undefined>[]);
 }

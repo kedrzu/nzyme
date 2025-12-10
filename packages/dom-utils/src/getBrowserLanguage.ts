eb5b8ebd52737f29a015e01af0a044bd132afe95
Michał Kędrzyński <kedrzu@sigma.clinic>
2025-12-10 10:01:34 -0800

    [SIG-251] Submodule changes

--- packages/dom-utils/src/getBrowserLanguage.ts
+++ packages/dom-utils/src/getBrowserLanguage.ts
@@ -7,8 +7,15 @@
  * @returns The best matching language code
  * @__NO_SIDE_EFFECTS__
  */
-export function getBrowserLanguage<const L extends readonly string[]>(availableLanguages: L): L[number] | undefined {
-    if (typeof navigator === 'undefined') {
+export function getBrowserLanguage<const L extends readonly string[]>(
+    availableLanguages: L,
+    navigator?: NavigatorLanguage,
+): L[number] | undefined {
+    if (!navigator && typeof window !== 'undefined') {
+        navigator = window.navigator;
+    }
+
+    if (!navigator) {
         return;
     }
 

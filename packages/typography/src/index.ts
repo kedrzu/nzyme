5da7782226be7b90e54f3ccc1d5674d35ff9d897
Michał Kędrzyński <kedrzu@sigma.clinic>
2026-06-24 10:40:05 +0200

    feat(typography): extract sanitizeText helper

--- packages/typography/src/index.ts
+++ packages/typography/src/index.ts
@@ -1,5 +1,6 @@
 export * from './fixOrphans.js';
 export * from './parseMarkdown.js';
 export * from './remarkSanitize.js';
+export * from './sanitizeText.js';
 export * from './stringifyMarkdown.js';
 export * from './types.js';
